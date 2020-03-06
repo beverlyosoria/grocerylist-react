dc53abc673956764aa9ad9ef977bfb5f6be7ba34
beverlyosoria <bevosoria@gmail.com>
2020-03-06 16:02:08 -0600

    sets state, sets up input and add button

--- src/App.js
+++ src/App.js
@@ -3,44 +3,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-	state = {
-		todos: [ 'First Item', 'Second item' ],
-		inputValue: ''
-	};
-	//the only thing this should handle is the todo to state
-	handleCreateTodo = (todo) => {
-		this.setState({
-			todos: [ todo, ...this.state.todos ]
-		});
-	};
-	handleSubmit = (event) => {
-		event.preventDefault();
-		this.handleCreateTodo(this.state.inputValue);
-		this.setState({
-			inputValue: ''
-		});
-	};
-	handleOnChange = (event) => {
-		this.setState({
-			inputValue: event.target.value
-		});
-	};
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			newItem: '',
+			list: []
+		};
+	}
 	render() {
-		const renderedTodos = this.state.todos.map((item, index) => {
-			return (
-				<li key={index} className="todo">
-					{item}
-				</li>
-			);
-		});
 		return (
 			<div className="App">
-				<ul className="todos">{renderedTodos}</ul>
-				<form className="add-form" onSubmit={this.handleSubmit}>
-					<input type="text" onChange={this.handleOnChange} value={this.state.inputValue} />
-					<input type="submit" />
-				</form>
+				<div>
+					Add an Item...
+					<br />
+				</div>
+				<input
+					type="text"
+					placeholder="Type item here..."
+					value={this.state.newItem}
+					onChange={(e) => this.updateInput('newItem', e.target.value)}
+				/>
+				<button onclick={() => this.addItem()}>Add</button>
 			</div>
 		);
 	}
